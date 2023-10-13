16f6e3c661d86cdedaca264420060600787ceb85
ethanlacerenza <71321892+ethanlacerenza@users.noreply.github.com>
2023-10-13 17:00:12 +0200

    using gpg for multi-platorm check

--- cmd/behavior-analysis/main.go
+++ cmd/behavior-analysis/main.go
@@ -1,100 +1,101 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "os"
-    "os/exec"
-    "bytes"
-    "net/http"
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
 )
 
 var apiKey string
 var filePath string
 
 func main() {
-    // Chiedi all'utente di inserire la sua API key
-    fmt.Print("Inserisci la tua API key di VirusTotal: ")
-    _, err := fmt.Scan(&apiKey)
-    if err != nil {
-        fmt.Println("Errore durante la lettura dell'API key:", err)
-        return
-    }
+	// Chiedi all'utente di inserire la sua API key
+	fmt.Print("Inserisci la tua API key di VirusTotal: ")
+	_, err := fmt.Scan(&apiKey)
+	if err != nil {
+		fmt.Println("Errore durante la lettura dell'API key:", err)
+		return
+	}
 
-    // Chiedi all'utente di inserire il percorso del file da analizzare
-    fmt.Print("Inserisci il percorso del file da analizzare: ")
-    _, err = fmt.Scan(&filePath)
-    if err != nil {
-        fmt.Println("Errore durante la lettura del percorso del file:", err)
-        return
-    }
+	// Chiedi all'utente di inserire il percorso del file da analizzare
+	fmt.Print("Inserisci il percorso del file da analizzare: ")
+	_, err = fmt.Scan(&filePath)
+	if err != nil {
+		fmt.Println("Errore durante la lettura del percorso del file:", err)
+		return
+	}
 
-    // Effettua la scansione del file con VirusTotal
-    virusTotalReport, err := scanFileWithVirusTotal(filePath)
-    if err != nil {
-        fmt.Println("Errore nella scansione del file con VirusTotal:", err)
-        return
-    }
+	// Effettua la scansione del file con VirusTotal
+	virusTotalReport, err := scanFileWithVirusTotal(filePath)
+	if err != nil {
+		fmt.Println("Errore nella scansione del file con VirusTotal:", err)
+		return
+	}
 
-    // Verifica la firma del file
-    signatureResult, err := analyzeFileSignature(filePath)
-    if err != nil {
-        fmt.Println("Errore durante l'analisi della firma del file:", err)
-        return
-    }
+	// Verifica la firma del file
+	signatureResult, err := verifyFileSignature(filePath)
+	if err != nil {
+		fmt.Println("Errore durante la verifica della firma del file:", err)
+		return
+	}
 
-    fmt.Println("Report di scansione di VirusTotal:")
-    fmt.Println(virusTotalReport)
-    fmt.Println("Risultato dell'analisi della firma del file:")
-    fmt.Println(signatureResult)
+	fmt.Println("Report di scansione di VirusTotal:")
+	fmt.Println(virusTotalReport)
+	fmt.Println("Risultato della verifica della firma del file:")
+	fmt.Println(signatureResult)
 }
 
 func scanFileWithVirusTotal(filePath string) (string, error) {
-    url := "https://www.virustotal.com/vtapi/v2/file/scan"
+	url := "https://www.virustotal.com/vtapi/v2/file/scan"
 
-    file, err := os.Open(filePath)
-    if err != nil {
-        return "", err
-    }
-    defer file.Close()
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
 
-    fileContents, err := io.ReadAll(file)
-    if err != nil {
-        return "", err
-    }
+	fileContents, err := io.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
 
-    body := bytes.NewBuffer(fileContents)
-    req, err := http.NewRequest("POST", url, body)
-    if err != nil {
-        return "", err
-    }
+	body := bytes.NewBuffer(fileContents)
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return "", err
+	}
 
-    req.Header.Set("x-apikey", apiKey)
+	req.Header.Set("x-apikey", apiKey)
 
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        return "", err
-    }
-    defer resp.Body.Close()
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
 
-    responseContents, err := io.ReadAll(resp.Body)
-    if err != nil {
-        return "", err
-    }
+	responseContents, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
 
-    return string(responseContents), nil
+	return string(responseContents), nil
 }
 
-func analyzeFileSignature(filePath string) (string, error) {
-    cmd := exec.Command("sigcheck", "-q", "-e", filePath)
+func verifyFileSignature(filePath string) (string, error) {
+	// Esegui un comando per verificare la firma del file con gpg
+	cmd := exec.Command("gpg", "--verify", filePath)
 
-    output, err := cmd.Output()
-    if err != nil {
-        return "", err
-    }
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
 
-    result := string(output)
+	result := string(output)
 
-    return result, nil
+	return result, nil
 }
